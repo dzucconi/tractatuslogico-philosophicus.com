ed2a35aebe5622d47a446161ea961293b0f251d1
dzucconi <damonzucconi@gmail.com>
2013-06-11 16:29:21 -0400

    A more object-oriented approach

--- application.rb
+++ application.rb
@@ -1,39 +1,67 @@
-class Application < Sinatra::Base
-  use Rack::Static, urls: ["/stylesheets"], root: "public"
+module Tractatus
+  class Tree
+    attr_reader :root, :lines
 
-  helpers do
-    def build_tree(lines)
-      root = Tree::TreeNode.new("Tractatus Logico-Philosophicus")
+    def initialize(lines)
+      @root = ::Tree::TreeNode.new("Tractatus Logico-Philosophicus")
+      @lines = lines
 
+      build!
+    end
+
+    def build!
       prev_depth = 0
       prev_node = root
 
       lines.each do |line|
-        match = /^\d.\d*/.match(line)
+        line = Line.new(line)
 
-        cur_depth = match.to_s.split(".")[1].try(:size) || 0
-        cur_node = Tree::TreeNode.new(match.to_s, match.post_match.strip)
-
-        # Determine number of parents to traverse
-        chain = (prev_depth - (cur_depth - 1)).times.collect { "parent" }
-
+        # Traverse up to appropriate parent
+        chain = (prev_depth - (line.depth - 1)).times.collect { "parent" }
         node = chain.inject(prev_node, &:send)
 
         # If node is nil then we are grafting onto the root node
-        (node.nil? ? root : node) << cur_node
+        (node.nil? ? root : node) << line.node
 
         # Setup for next loop
-        prev_depth = cur_depth
-        prev_node = cur_node
+        prev_depth = line.depth
+        prev_node = line.node
       end
+    end
+  end # Tree
 
-      root
-    end # build_tree
-  end # helpers
+  class Line
+    attr_reader :line, :node
+
+    def initialize(line)
+      @line = line
+      @node = ::Tree::TreeNode.new(name, statement)
+    end
+
+    def match
+      @match ||= /^\d.\d*/.match(line)
+    end
+
+    def name
+      @name ||= match.to_s
+    end
+
+    def depth
+      @depth ||= match.to_s.split(".")[1].try(:size) || 0
+    end
+
+    def statement
+      @statement ||= match.post_match.strip
+    end
+  end # Line
+end # Tractatus
+
+class Application < Sinatra::Base
+  use Rack::Static, urls: ["/stylesheets"], root: "public"
 
   get "/" do
     @lines = File.open("./resources/txt/tractatus.txt").read.split("\n")
-    @tree = build_tree(@lines)
+    @tractatus = Tractatus::Tree.new(@lines)
 
     erb :index
   end
