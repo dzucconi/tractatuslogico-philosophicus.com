3d90aea63951e0bcaedd65bb8a37b55aabd9dfb7
dzucconi <damonzucconi@gmail.com>
2013-06-11 23:20:03 -0400

    Reorganize application

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,38 +1,38 @@
 (function(){
   "use-strict";
 
-  var Tractatus = {
-    foldAndAttach: function($el) {
-      $el.find("li > ol").hide().each(function(i) {
-        var $parentLi, $childOl;
+  var App = {
+    views: {
+      foldAndAttach: function($el) {
+        $el.find("li > ol").hide().each(function(i) {
+          var $parentLi, $childOl;
 
-        $parentLi = $(this).parent("li");
-        $childOl = $(this).remove();
+          $parentLi = $(this).parent("li");
+          $childOl = $(this).remove();
 
-        $parentLi.find(".content").on("click", function() {
-          $childOl.toggle();
-          $parentLi.toggleClass("is-open");
+          $parentLi.find(".content").on("click", function() {
+            $childOl.toggle();
+            $parentLi.toggleClass("is-open");
+          });
+
+          $parentLi.append($childOl);
+        });
+      },
+
+      attachMenu: function($el) {
+        $el.find("#expand").on("click", function() {
+          $("ol").show();
         });
 
-        $parentLi.append($childOl);
-      });
+        $el.find("#collapse").on("click", function() {
+          $("li > ol").hide();
+        });
+      }
     },
 
-    attachMenu: function($el) {
-      $el.find("#expand").on("click", function() {
-        $("ol").show();
-      });
-
-      $el.find("#collapse").on("click", function() {
-        $("li > ol").hide();
-      });
-    }
-  };
-
-  var App = {
     initialize: function() {
-      Tractatus.foldAndAttach($("#root"));
-      Tractatus.attachMenu($("#menu"));
+      App.views.foldAndAttach($("#root"));
+      App.views.attachMenu($("#menu"));
     }
   };
 
